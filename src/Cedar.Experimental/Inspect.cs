977930cc316c1823f6ef70e50c7e93af04ac011f
Ryan Harris <ryan.harris@strongdm.com>
2026-03-28 07:20:56 -0700

    semport: implement d0c76a5 - AstInspect.Inspect walk function for Cedar.Experimental

---
+++ src/Cedar.Experimental/Inspect.cs
@@ -0,0 +1,157 @@
+using System;
+using Cedar.Ast;
+using Cedar.Ast.Internal;
+
+namespace Cedar.Experimental;
+
+public static class AstInspect
+{
+    public static void Inspect(Node node, Func<INode, bool> fn)
+    {
+        ArgumentNullException.ThrowIfNull(node);
+        ArgumentNullException.ThrowIfNull(fn);
+
+        InspectNode(node.Inner, fn);
+    }
+
+    private static void InspectNode(INode? node, Func<INode, bool> fn)
+    {
+        if (node is null)
+        {
+            return;
+        }
+
+        if (!fn(node))
+        {
+            return;
+        }
+
+        switch (node)
+        {
+            case NodeIfThenElse ifThenElse:
+                InspectNode(ifThenElse.If, fn);
+                InspectNode(ifThenElse.Then, fn);
+                InspectNode(ifThenElse.Else, fn);
+                break;
+            case NodeOr or:
+                InspectNode(or.Left, fn);
+                InspectNode(or.Right, fn);
+                break;
+            case NodeAnd and:
+                InspectNode(and.Left, fn);
+                InspectNode(and.Right, fn);
+                break;
+            case NodeLessThan lessThan:
+                InspectNode(lessThan.Left, fn);
+                InspectNode(lessThan.Right, fn);
+                break;
+            case NodeLessThanOrEqual lessThanOrEqual:
+                InspectNode(lessThanOrEqual.Left, fn);
+                InspectNode(lessThanOrEqual.Right, fn);
+                break;
+            case NodeGreaterThan greaterThan:
+                InspectNode(greaterThan.Left, fn);
+                InspectNode(greaterThan.Right, fn);
+                break;
+            case NodeGreaterThanOrEqual greaterThanOrEqual:
+                InspectNode(greaterThanOrEqual.Left, fn);
+                InspectNode(greaterThanOrEqual.Right, fn);
+                break;
+            case NodeNotEquals notEquals:
+                InspectNode(notEquals.Left, fn);
+                InspectNode(notEquals.Right, fn);
+                break;
+            case NodeEquals equals:
+                InspectNode(equals.Left, fn);
+                InspectNode(equals.Right, fn);
+                break;
+            case NodeIn @in:
+                InspectNode(@in.Left, fn);
+                InspectNode(@in.Right, fn);
+                break;
+            case NodeHasTag hasTag:
+                InspectNode(hasTag.Left, fn);
+                InspectNode(hasTag.Right, fn);
+                break;
+            case NodeGetTag getTag:
+                InspectNode(getTag.Left, fn);
+                InspectNode(getTag.Right, fn);
+                break;
+            case NodeContains contains:
+                InspectNode(contains.Left, fn);
+                InspectNode(contains.Right, fn);
+                break;
+            case NodeContainsAll containsAll:
+                InspectNode(containsAll.Left, fn);
+                InspectNode(containsAll.Right, fn);
+                break;
+            case NodeContainsAny containsAny:
+                InspectNode(containsAny.Left, fn);
+                InspectNode(containsAny.Right, fn);
+                break;
+            case NodeAdd add:
+                InspectNode(add.Left, fn);
+                InspectNode(add.Right, fn);
+                break;
+            case NodeSub sub:
+                InspectNode(sub.Left, fn);
+                InspectNode(sub.Right, fn);
+                break;
+            case NodeMult mult:
+                InspectNode(mult.Left, fn);
+                InspectNode(mult.Right, fn);
+                break;
+            case NodeHas has:
+                InspectNode(has.Arg, fn);
+                break;
+            case NodeAccess access:
+                InspectNode(access.Arg, fn);
+                InspectNode(access.Attribute, fn);
+                break;
+            case NodeLike like:
+                InspectNode(like.Arg, fn);
+                break;
+            case NodeIs @is:
+                InspectNode(@is.Left, fn);
+                break;
+            case NodeIsIn isIn:
+                InspectNode(isIn.Left, fn);
+                InspectNode(isIn.Entity, fn);
+                break;
+            case NodeNegate negate:
+                InspectNode(negate.Arg, fn);
+                break;
+            case NodeNot not:
+                InspectNode(not.Arg, fn);
+                break;
+            case NodeIsEmpty isEmpty:
+                InspectNode(isEmpty.Arg, fn);
+                break;
+            case NodeExtensionCall extensionCall:
+                foreach (INode arg in extensionCall.Args)
+                {
+                    InspectNode(arg, fn);
+                }
+
+                break;
+            case NodeRecord record:
+                foreach (NodeRecordElement element in record.Elements)
+                {
+                    InspectNode(element.Value, fn);
+                }
+
+                break;
+            case NodeSet set:
+                foreach (INode element in set.Elements)
+                {
+                    InspectNode(element, fn);
+                }
+
+                break;
+            case NodeValue:
+            case NodeVariable:
+            default:
+                break;
+        }
+    }
+}
---
+++ test/Cedar.Experimental.Tests/InspectTests.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using Cedar.Ast;
+using Cedar.Ast.Internal;
+using Cedar.Experimental;
+using Cedar.Types;
+using Xunit;
+
+namespace Cedar.Experimental.Tests;
+
+public sealed class InspectTests
+{
+    public static IEnumerable<object[]> CountCases()
+    {
+        INode leaf1 = Values.Long(1).Inner;
+        INode leaf2 = Values.Long(2).Inner;
+
+        yield return ["IfThenElse", Wrap(new NodeIfThenElse(leaf1, leaf1, leaf1)), 4];
+        yield return ["Or", Wrap(new NodeOr(leaf1, leaf2)), 3];
+        yield return ["And", Wrap(new NodeAnd(leaf1, leaf2)), 3];
+        yield return ["LessThan", Wrap(new NodeLessThan(leaf1, leaf2)), 3];
+        yield return ["LessThanOrEqual", Wrap(new NodeLessThanOrEqual(leaf1, leaf2)), 3];
+        yield return ["GreaterThan", Wrap(new NodeGreaterThan(leaf1, leaf2)), 3];
+        yield return ["GreaterThanOrEqual", Wrap(new NodeGreaterThanOrEqual(leaf1, leaf2)), 3];
+        yield return ["NotEquals", Wrap(new NodeNotEquals(leaf1, leaf2)), 3];
+        yield return ["Equals", Wrap(new NodeEquals(leaf1, leaf2)), 3];
+        yield return ["In", Wrap(new NodeIn(leaf1, leaf2)), 3];
+        yield return ["HasTag", Wrap(new NodeHasTag(leaf1, leaf2)), 3];
+        yield return ["GetTag", Wrap(new NodeGetTag(leaf1, leaf2)), 3];
+        yield return ["Contains", Wrap(new NodeContains(leaf1, leaf2)), 3];
+        yield return ["ContainsAll", Wrap(new NodeContainsAll(leaf1, leaf2)), 3];
+        yield return ["ContainsAny", Wrap(new NodeContainsAny(leaf1, leaf2)), 3];
+        yield return ["Add", Wrap(new NodeAdd(leaf1, leaf2)), 3];
+        yield return ["Sub", Wrap(new NodeSub(leaf1, leaf2)), 3];
+        yield return ["Mult", Wrap(new NodeMult(leaf1, leaf2)), 3];
+        yield return ["Has", Wrap(new NodeHas(leaf1, new CedarString("a"))), 2];
+        yield return ["Access", Wrap(new NodeAccess(leaf1, leaf2)), 3];
+        yield return ["Like", Wrap(new NodeLike(leaf1, new CedarPattern(Wildcard.Instance))), 2];
+        yield return ["Is", Wrap(new NodeIs(leaf1, new CedarPath("T"))), 2];
+        yield return ["IsIn", Wrap(new NodeIsIn(leaf1, new CedarPath("T"), leaf2)), 3];
+        yield return ["Negate", Wrap(new NodeNegate(leaf1)), 2];
+        yield return ["Not", Wrap(new NodeNot(leaf1)), 2];
+        yield return ["IsEmpty", Wrap(new NodeIsEmpty(leaf1)), 2];
+        yield return ["ExtensionCall", Wrap(new NodeExtensionCall(new CedarPath("f"), ImmutableArray.Create(leaf1, leaf2))), 3];
+        yield return ["Record", Wrap(new NodeRecord(ImmutableArray.Create(new NodeRecordElement(new CedarString("k"), leaf1)))), 2];
+        yield return ["Set", Wrap(new NodeSet(ImmutableArray.Create(leaf1, leaf2))), 3];
+        yield return ["Variable", Wrap(new NodeVariable(new CedarString("v"))), 1];
+        yield return ["Value", Wrap(new NodeValue(new CedarLong(1))), 1];
+    }
+
+    [Theory]
+    [MemberData(nameof(CountCases))]
+    public void Inspect_CountsNodesForEachNodeKind(string _, Node node, int expected)
+    {
+        Assert.Equal(expected, CountNodes(node));
+    }
+
+    [Fact]
+    public void Inspect_SkipsChildren_WhenCallbackReturnsFalse()
+    {
+        Node leaf = Values.Long(1);
+        Node root = leaf.And(leaf);
+        int count = 0;
+
+        AstInspect.Inspect(root, node =>
+        {
+            count++;
+            return node is not NodeAnd;
+        });
+
+        Assert.Equal(1, count);
+    }
+
+    private static int CountNodes(Node node)
+    {
+        int count = 0;
+
+        AstInspect.Inspect(node, _ =>
+        {
+            count++;
+            return true;
+        });
+
+        return count;
+    }
+
+    private static Node Wrap(INode node)
+    {
+        return new Node(node);
+    }
+}
